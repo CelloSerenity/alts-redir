bcef9b38d094045d7a06386aa4233d0b4c1d3118
CelloSerenity <195480169+CelloSerenity@users.noreply.github.com>
2025-11-03 08:01:29 -0700

    Update redirect.js
    Signed-off-by: CelloSerenity <195480169+CelloSerenity@users.noreply.github.com>

--- redirect.js
+++ redirect.js
@@ -1,2 +1,11 @@
-const r = new URLSearchParams(window.location.search).get("r")
-if (r) window.location.replace(r)
+const sp = new URLSearchParams(window.location.search)
+const r = (sp.get("r") || "").toLowerCase()
+const sourceUrl = sp.get("url")
+if (r && sourceUrl) {
+  let target = ""
+  if (r === "sidestore") target = "sidestore://source?url=" + sourceUrl
+  else if (r === "altstore") target = "altstore-classic://source?url=" + sourceUrl
+  else if (r === "feather") target = "feather://source/" + sourceUrl
+  else if (r === "livecontainer") target = "livecontainer://source?url=" + sourceUrl
+  if (target) window.location.replace(target)
+}
